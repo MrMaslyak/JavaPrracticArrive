f9f01fd7f18ba76dcee402b849a6d887ed647f70
Ilya Maslyanyi <maslanyjila@gmail.com>
2024-05-06 00:04:35 +0300

    first commit

--- src/Main.java
+++ src/Main.java
@@ -14,17 +14,19 @@ public class Main {
         System.out.println("Your arrive: " + Arrays.toString(massive));
         minVolume( massive);
         maxVolume(massive);
+        AvarageVolume(massive);
+        NumbersMoreThanFive(massive);
     }
 
     public static void minVolume (int[] massive){
-    int min = massive[0];
+        int min = massive[0];
         for (int i = 0; i < massive.length; i++) {
             if (min>massive[i]){
                 min = massive[i];
             }
         }
-            System.out.println("Minimal number in your arrive: " + min);
-        }
+        System.out.println("Minimal number in your arrive: " + min);
+    }
     public static void maxVolume (int[] massive){
         int max = massive[0];
         for (int i = 0; i < massive.length; i++) {
@@ -34,4 +36,30 @@ public class Main {
         }
         System.out.println("Maximal number in your arrive: " + max);
     }
+    public static void AvarageVolume(int[] massive){
+        int sum = massive[0];
+        for (int i = 0; i < massive.length; i++) {
+            sum =+ massive[i];
+        }
+        double average = (double) sum / massive.length;
+        System.out.println("Average number in your arrive: " + average);
     }
+    public static void NumbersMoreThanFive(int[] massive) {
+        System.out.print("Numbers more than five: ");
+        boolean found = false;
+        for (int i = 0; i < massive.length; i++) {
+            if (massive[i] > 5) {
+                if (found) {
+                    System.out.print(", ");
+                }
+                System.out.print(massive[i]);
+                found = true;
+            }
+        }
+        if (!found) {
+            System.out.print("None");
+        }
+        System.out.println();
+    }
+
+}
