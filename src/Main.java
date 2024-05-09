8724ea20b4093bd16902ccd06c4f1adf26a1970f
Ilya Maslyanyi <maslanyjila@gmail.com>
2024-05-09 13:50:32 +0300

    firt commit

--- src/Main.java
+++ src/Main.java
@@ -16,6 +16,10 @@ public class Main {
         maxVolume(massive);
         AvarageVolume(massive);
         NumbersMoreThanFive(massive);
+        InsertSort(massive);
+        SelectSort(massive);
+        BubbleSort(massive);
+//        HardTask();
     }
 
     public static void minVolume (int[] massive){
@@ -60,6 +64,52 @@ public class Main {
             System.out.print("None");
         }
         System.out.println();
+    }
+
+    public static void HardTask(){
+        int[] arrive = {1,2,3,4,5,6,7,8,8,10};
+        for (int i = 0; i < arrive.length; i++) {
+            arrive[i] = i;
+        }
+        System.out.println(Arrays.toString(arrive));
+    }
+
+    public static void InsertSort (int [] massive){
+        for (int i = 0; i < massive.length ; i++) {
+            int tmp = massive[i];
+            int j = i - 1;
+            while (j >= 0 && massive[j] > tmp) {
+            massive[j+1] = massive[j];
+            j -- ;
+            }
+            massive[j+1] = tmp;
+        }
+        System.out.println("Insert-sort your arrives: " + Arrays.toString(massive));
+    }
+
+    public static void SelectSort (int [] massive){
+        for (int i = 0; i < massive.length; i++) {
+            for (int j = i+1; j < massive.length ; j++) {
+                if (massive[i]>massive[j]){
+                    int tmp = massive[i];
+                    massive[i]=massive[j];
+                    massive[j] = tmp;
+                }
+            }
+        }
+        System.out.println("Select-sort your arrives: " + Arrays.toString(massive));
+    }
+    public static void BubbleSort (int [] massive){
+        for (int i = 0; i < massive.length; i++) {
+            for (int j = 0; j < massive.length - 1 ; j++) {
+                if (massive[j]>massive[j+1]){
+                    int tmp = massive[j];
+                    massive[j]=massive[j+1];
+                    massive[j+1] = tmp;
+                }
+            }
+        }
+        System.out.println("Bubble-sort your arrives: " + Arrays.toString(massive));
     }
 
 }
